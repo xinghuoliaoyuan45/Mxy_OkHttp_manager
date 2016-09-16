723c7df4559c4aefaa3fff1b7f32b63eabad4c43
xinghuoliaoyuan45 <18749818136@163.com>
2016-09-16 17:07:48 +0800

    提交数据到本地tomcat

--- app/src/main/java/com/okhttp/mxy/okhttp_mxymanage/MainActivity.java
+++ app/src/main/java/com/okhttp/mxy/okhttp_mxymanage/MainActivity.java
@@ -9,12 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private  final  static String TAG = MainActivity.class.getSimpleName();
     private String image_path = "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white_fe6da1ec.png";
+    private String form_path="http://192.168.56.1:8080/mxy_webproject/LoginAction";
     private OkHttpManager okHttpManager;
     private ImageView imageView;
     private  Button button;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +28,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         button=(Button) findViewById(R.id.button);
         imageView=(ImageView)findViewById(R.id.img);
+        button1=(Button) findViewById(R.id.button1);
 
         button.setOnClickListener(this);
+        button1.setOnClickListener(this);
         okHttpManager=OkHttpManager.getInstance();
 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(final View v) {
         switch (v.getId()){
             case R.id.button:
                 okHttpManager.asyncByteByUrl(image_path, new OkHttpManager.Func() {
@@ -38,6 +46,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         Log.i(TAG, String.valueOf(result));
                         Bitmap bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
                         imageView.setImageBitmap(bitmap);
+                    }
+                });
+            case  R.id.button1:
+                HashMap<String, String> map = new HashMap<String,String>();
+                map.put("username" , "admin");
+                map.put("password" , "12345");
+                okHttpManager.sendComplexForm(form_path, map , new OkHttpManager.Func1() {
+                    @Override
+                    public void onResponse(String values) {
+                        Log.i(TAG,values);
                     }
                 });
         }
--- app/src/main/java/com/okhttp/mxy/okhttp_mxymanage/OkHttpManager.java
+++ app/src/main/java/com/okhttp/mxy/okhttp_mxymanage/OkHttpManager.java
@@ -4,12 +4,17 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
@@ -67,16 +72,46 @@ public class OkHttpManager {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
+                Log.i(TAG,"失败图片");
                 e.printStackTrace();
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if(response!=null&&response.isSuccessful()){
+                    Log.i(TAG,"图片数据返回来数据了");
                     onSuccessByteMethod(response.body().bytes(),callBack);
                 }
             }
         });
+    }
+
+    public void sendComplexForm(String url,Map<String,String> params,final Func1 callBack ){
+        FormBody.Builder form_builder=new FormBody.Builder();
+        Log.i(TAG,"走到这里1");
+        if(params != null && !params.isEmpty()){
+            for(Map.Entry<String,String> entry:params.entrySet()){
+                form_builder.add(entry.getKey(),entry.getValue());
+                Log.i(TAG,"走到这里2");
+            }
+        }
+        final RequestBody requestBody=form_builder.build();
+        final Request request=new Request.Builder().url(url).post(requestBody).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response!=null&&response.isSuccessful()) {
+                    Log.i(TAG,"tomcat返回来数据了");
+                    onSuccessStringMethod(response.body().string(),callBack);
+                }
+            }
+        });
+
+
     }
 
     /**
@@ -101,9 +136,28 @@ public class OkHttpManager {
             }
         });
     }
+    private void onSuccessStringMethod(final String value, final Func1 callBack){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if(callBack!=null){
+                    try{
+                        callBack.onResponse(value);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
     
     interface  Func{
         void onResponse(byte[] result);
+    }
+    interface  Func1{
+        void onResponse(String values);
     }
 
 
